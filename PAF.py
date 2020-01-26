38ec77182663aff32592754f16674e6593db38fc
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2020-01-26 15:40:12 +0100

    Last update
    or not

--- PAF.py
+++ PAF.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from scipy import signal
 from scipy.io.wavfile import read, write
+import numpy as np
 import matplotlib.pyplot as plt
 import wave, argparse, sys, time
 
@@ -9,32 +10,30 @@ def parser():
     parser.add_argument("INPUT", help="Name of the unfiltered audio.")
     parser.add_argument("-l", "--lowpass", help="Low pass filter. Default value: 5000.", type=int)
     parser.add_argument("-hi", "--highpass", help="High pass filter. Default value: 5000.", type=int)
-    parser.add_argument("-bp", "--bandpass", help="Band pass filter. Default value: [5000,10000].", type=int)
-    parser.add_argument("-bs", "--bandstop", help="Band Stop filter. Default value: [5000,10000].", type=int)
+    parser.add_argument("-bp", "--bandpass", help="Band pass filter. Default value: [5000,10000].", nargs='+', type=int)
+    parser.add_argument("-bs", "--bandstop", help="Band Stop filter. Default value: [5000,10000].", type=list)
     parser.add_argument("-o", "--output", help="Name of the output wav file. Default value: output.wav.")
     parser.add_argument("-s", "--sampling", help="Sampling rate. Default value: same as input.", type=int)
     args = parser.parse_args()
 
-    filterfreq = [5000]
+    filterfreq = [5000,10000]
     output = "output.wav"
 
     if args.output:
         output = args.output
-    else if args.lowpass:
+    elif args.lowpass:
         filterfreq[0] = args.lowpass
         filtertype = "lowpass"
-    else if args.highpass:
+    elif args.highpass:
         filterfreq[0] = args.highpass
         filtertype = "highpass"
-    else if args.bandpass:
-        filterfreq[1] = [10000]
+    elif args.bandpass:
         filterfreq = args.bandpass
         filtertype = "bandpass"
-    else if args.bandstop:
-        filterfreq[1] = [10000]
+    elif args.bandstop:
         filterfreq = args.bandstop
         filtertype = "bandstop"
-    else :
+    else:
         print("You have to choose a filter.")
         time.sleep(2)
         sys.exit()
@@ -43,11 +42,12 @@ def parser():
     samplingrate = wave.Wave_read.getframerate(original)
     nyqfreq = samplingrate / 2
     original.close()
-
-    if args.bandpass == True or args.bandstop == True :
-        cutoff[0] = filterfreq[0] / nyqfreq
-        cutoff[1] = filterfreq[0] / nyqfreq
-    else :
+    print(filterfreq[0])
+    print(filtertype)
+    if (filtertype == "bandstop") or (filtertype =="bandpass"):
+        cutoff = [filterfreq[0]/nyqfreq, filterfreq[1]/nyqfreq]
+        print(cutoff)
+    else:
         cutoff = filterfreq[0] / nyqfreq
 
     if args.sampling:
@@ -57,23 +57,22 @@ def parser():
     print('Output file: %s.' % output)
     print('Sampling rate: %d.' % samplingrate)
     if args.lowpass :
-        print('Low pass filter: %d.' % filterfreq)
+        print('Low pass filter: %d.' % filterfreq[0])
     if args.highpass :
-        print('High pass filter: %d.' % filterfreq)
+        print('High pass filter: %d.' % filterfreq[0])
     if args.bandpass :
-        print('Band pass filter: %d.' % filterfreq)
+        print('Band pass filter: %d - %d.' % (filterfreq[0],filterfreq[1]))
     if args.bandstop :
-        print('Band stop filter: %d.' % filterfreq)
+        print('Band stop filter: %d - %d.' % (filterfreq[0],filterfreq[1]))
 
     return (args.INPUT, output, filtertype, samplingrate, cutoff, nyqfreq)
 
-def filter(inpt, output, filtertype, samplingrate, cutoff, nyqfreq):
+def filter(inpt, output, filtertype, samp, cutoff, nyqfreq):
     input_data = read(inpt)
     audio = input_data[1]
-    b, a = signal.butter(8, cutoff, btype=filtertype)
-    filteredaudio = signal.filtfilt(b, a, audio)
-    write(output, samplingrate, filteredaudio)
-    write("output.wav", samplingrate, output)
+    b, a = signal.butter(5, cutoff, btype=filtertype)
+    filteredaudio = signal.lfilter(b, a, audio)
+
 
     ### Screens
     #Comparison
@@ -84,13 +83,14 @@ def filter(inpt, output, filtertype, samplingrate, cutoff, nyqfreq):
     plt.ylabel('Range')
     plt.legend()
     plt.savefig('comparison.png')
+    plt.close()
 
     ### Screens
     # Comparison
-    f, FFT = signal.periodogram(audio, samplingrate)
-    e, EET = signal.periodogram(filteredaudio, samplingrate)
-    plt.plot(f, FFT)
-    plt.plot(e, EET)
+    f, FFT = signal.periodogram(audio, samp)
+    e, EET = signal.periodogram(filteredaudio, samp)
+    plt.plot(f,FFT)
+    plt.plot(e,EET)
     plt.xlabel('Freq (Hz)')
     plt.ylabel('Module')
     plt.xlim(0, nyqfreq)
@@ -98,6 +98,7 @@ def filter(inpt, output, filtertype, samplingrate, cutoff, nyqfreq):
     plt.grid()
     plt.title('Freq visualisation', fontsize=14)
     plt.savefig('comparison2.png')
+    plt.close()
     # Original only
     plt.plot(f, FFT)
     plt.xlabel('Freq (Hz)')
@@ -107,6 +108,7 @@ def filter(inpt, output, filtertype, samplingrate, cutoff, nyqfreq):
     plt.grid()
     plt.title('Freq visualisation', fontsize=14)
     plt.savefig('original.png')
+    plt.close()
     # Filtered only
     plt.plot(e, EET)
     plt.xlabel('Freq (Hz)')
@@ -116,6 +118,17 @@ def filter(inpt, output, filtertype, samplingrate, cutoff, nyqfreq):
     plt.grid()
     plt.title('Freq visualisation', fontsize=14)
     plt.savefig('filtered.png')
+    plt.close()
+    Outputfile = np.fft.ifft(FFT)
+    plt.plot(Outputfile)
+    plt.ylabel('Amplitude')
+    plt.xlim(0, 2000)
+    plt.grid()
+    plt.title('Signal after filter', fontsize=14)
+    plt.savefig('Signal.png')
+    plt.close()
+    Outputfile = Outputfile.astype('uint8')
+    write(output, samp, Outputfile)
 
 
 if __name__ == '__main__':
