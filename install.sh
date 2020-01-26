41056ece5fbd12d9fa01c3bd6edc0c1300849444
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2020-01-26 16:57:31 +0100

    gotta update things

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # Updates everything
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt install python3-pip pkg-config python3-matplotlib -y
+sudo apt install python3-pip pkg-config python3-matplotlib python3-scipy -y
 # Install needed libraries
 pip3 install -r requirements.txt
